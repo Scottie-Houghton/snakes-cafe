78b58014ee8a97b94a9177ff1bea6775577962cf
Scottie <scott.c.houghton@gmail.com>
2022-07-19 15:16:04 -0700

    Created a dictionary of every item on the menu. Created an input function. Initiated a while loop that will run until quit is typed. Added if statement to tell customer to order from menu if they don't. If statement to add one item to the meal. Else statement to say more than one order of an item is added. Input statement at the end of the while loop so that it keeps prompting until quit is typed.

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -37,15 +37,43 @@ Unicorn Tears
 """)
 
 #intialize empty meal dictionary
-
-# start for loop here until user enters "quit"
-
-# create a variable to store the user's order
-order = input("> ")
+items = {
+    "Wings": 0,
+    "Cookies": 0,
+    "Spring Rolls": 0,
+    "Salmon": 0,
+    "Steak": 0,
+    "Meat Tornado": 0,
+    "A Literal Garden": 0,
+    "Ice Cream": 0,
+    "Cake": 0,
+    "Pie": 0,
+    "Coffee": 0,
+    "Tea": 0,
+    "Unicorn Tears": 0
+}
 
 # print the order
 num_items = 1 #TODO: properly tally items that have been ordered
-report = f"** {num_items} order of {order} have been added to your meal **"
+# food = []
+order = input("> ")
+while order != "quit":
+    if order not in items:
+        print("Please order from the menu.")
+        order = input("> ")
+        continue
+    if items[order] == 0:
+        items[order] = 1
+        report = f"** 1 order of {order} has been added to your meal **"
+        print(report)
+    # if order not in food:
+        # food.append(order)
+        # report = f"** {food.count(order)} order of {order} has been added to your meal **"
+        # print(report)
+    else:
+        items[order] += 1
+        report = f"** {items[order]} orders of {order} have been added to your meal **"
+        print(report)
 
-print(report)
-#end loop
+    order = input("> ")
+# end loop
